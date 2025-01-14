441bd01bbfe230c2113a7fa9ee5e718b926656e7
dajinYang <didekwls0104@naver.com>
2025-01-14 15:47:41 +0900

    [feat] BaseResponseDto 생성

--- src/main/java/sopt/jeolloga/common/ResponseDto.java
+++ src/main/java/sopt/jeolloga/common/ResponseDto.java
@@ -1,2 +1,19 @@
-package sopt.jeolloga.common;public class ResponseDto {
+package sopt.jeolloga.common;
+
+public record ResponseDto<T>(
+        String code,
+        T data,
+        String msg
+) {
+    public static <T> ResponseDto<T> fail(String code, String msg) {
+        return new ResponseDto<>(code, null, msg);
+    }
+
+    public static <T> ResponseDto<T> failValidate(final T data) {
+        return new ResponseDto<>("fail", data, null);
+    }
+
+    public static <T> ResponseDto<T> success(final T data) {
+        return new ResponseDto<>(null, data, null);
+    }
 }
