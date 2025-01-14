424e08e8305894dc64727f659faddb227ecfd12d
dajinYang <didekwls0104@naver.com>
2025-01-14 18:33:03 +0900

    [refactor] RequestMapping 어노테이션 제거

--- src/main/java/sopt/jeolloga/domain/templestay/api/controller/ReviewController.java
+++ src/main/java/sopt/jeolloga/domain/templestay/api/controller/ReviewController.java
@@ -10,18 +10,10 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/public/templestay")
 public class ReviewController {
 
     private final ReviewService reviewService;
-
-    /**
-     * Templestay ID를 기반으로 블로그 정보를 가져오는 API
-     *
-     * @param templestayId 템플스테이 ID
-     * @return 블로그 정보 리스트
-     */
-    @GetMapping("/{templestayId}/reviews")
+    @GetMapping("/public/templestay/{templestayId}/reviews")
     public ResponseEntity<List<TemplestayVO>> getBlogsByTemplestayId(@PathVariable Long templestayId) {
         List<TemplestayVO> blogs = reviewService.getBlogsByTemplestayId(templestayId);
         return ResponseEntity.ok(blogs);
