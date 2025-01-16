4791e9b8359e94a8ed5c049a095bb359b82b7ebd
Hoyoung027 <hybyun0207@gmail.com>
2025-01-17 06:36:20 +0900

    [feat] member api 구현 추가

--- src/main/java/sopt/jeolloga/domain/member/api/repository/Member.java
+++ src/main/java/sopt/jeolloga/domain/member/api/repository/Member.java
@@ -3,10 +3,12 @@ package sopt.jeolloga.domain.member.api.repository;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "member")
 @Getter
+@Setter
 @NoArgsConstructor
 public class Member {
 
