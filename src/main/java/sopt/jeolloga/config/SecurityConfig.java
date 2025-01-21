15e832aefc53fbda0c70e82aff49e7b262992fa8
dajinYang <didekwls0104@naver.com>
2025-01-21 22:47:21 +0900

    [fix] SecurityConfig import 추가

--- src/main/java/sopt/jeolloga/config/SecurityConfig.java
+++ src/main/java/sopt/jeolloga/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 
 import java.util.List;
